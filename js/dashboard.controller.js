c9ea754ea3aa2e2ed35365473f90d09327c5b87c
Vamsi <yechoorv@gmail.com>
2017-03-28 14:08:51 -0400

    tab formatting

--- js/dashboard.controller.js
+++ js/dashboard.controller.js
@@ -6,68 +6,63 @@ DashboardController.$inject = ['$scope', '$http'];
 
 function DashboardController($scope, $http) {
   var dashboard = $scope;
-  dashboard.hello = hello();
+  // TODO: change sessionStorage to actually retrieve variables
+  // dashboard.onyen = sessionStorage.getItem('uid');
+  // dashboard.pid = sessionStorage.getItem('pid');
+  // dashboard.firstName = sessionStorage.getItem('givenName');
+  // dashboard.lastName = sessionStorage.getItem('sn');
+  // dashboard.email = sessionStorage.getItem('mail');
+  dashboard.onyen = 'yechoorv';
 
-  function hello() {
-    // TODO: change sessionStorage to actually retrieve variables
-    // dashboard.onyen = sessionStorage.getItem('uid');
-    // dashboard.pid = sessionStorage.getItem('pid');
-    // dashboard.firstName = sessionStorage.getItem('givenName');
-    // dashboard.lastName = sessionStorage.getItem('sn');
-    // dashboard.email = sessionStorage.getItem('mail');
-    dashboard.onyen = 'yechoorv';
+  setAccess();
+  setUserInfo();
+  getAttendance(dashboard.onyen);
 
-    setAccess();
-    setUserInfo();
-    getAttendance(dashboard.onyen);
+  function setAccess(){
+    dashboard.isStudent = true;
+    dashboard.isInstructor = false;
+    dashboard.isAdministrator = false;
+  }
 
-    function setAccess(){
-      dashboard.isStudent = true;
-      dashboard.isInstructor = false;
-      dashboard.isAdministrator = false;
+  function setUserInfo() {
+    var http = $http;
+  }
+
+  function getAttendance(data) {
+    $http({
+      method: 'POST',
+      url: '/backend/getAttendance.php',
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      transformRequest: function(obj) {
+        var str = [];
+        for(var p in obj)
+        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        return str.join("&");
+      },
+      data: {onyen: data}
+    }).then(successCallback, errorCallback);
+
+    function successCallback(response) {
+      dashboard.records = response.data.result;
+      createTabs();
     }
 
-    function setUserInfo() {
-      var http = $http;
-    }
-
-    function getAttendance(data) {
-      $http({
-        method: 'POST',
-        url: '/backend/getAttendance.php',
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-        transformRequest: function(obj) {
-          var str = [];
-          for(var p in obj)
-          str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-          return str.join("&");
-        },
-        data: {onyen: data}
-      }).then(successCallback, errorCallback);
-
-      function successCallback(response) {
-        dashboard.records = response.data.result;
-        createTabs();
-      }
-
-      function errorCallback(response) {
-        alert("fail");
-        dashboard.records = [];
-        createTabs();
-      }
-    }
-
-    function createTabs() {
-      dashboard.tabs = {};
-      dashboard.records.forEach(function(value, key){
-          var courseName = value.course;
-          if(dashboard.tabs[courseName]){
-            dashboard.tabs[courseName].attendance++;
-          } else {
-            dashboard.tabs[courseName] = {attendance: 1, records: dashboard.records};
-          }
-      });
+    function errorCallback(response) {
+      alert("fail");
+      dashboard.records = [];
+      createTabs();
     }
   }
 
+  function createTabs() {
+    dashboard.tabs = {};
+    dashboard.records.forEach(function(value, key){
+      var courseName = value.course;
+      if(dashboard.tabs[courseName]){
+        dashboard.tabs[courseName].attendance++;
+      } else {
+        dashboard.tabs[courseName] = {attendance: 1, records: dashboard.records};
+      }
+    });
+  }
 }
