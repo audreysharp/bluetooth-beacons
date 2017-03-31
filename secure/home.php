8382517f7fa4d992e6c1fa6fd568a927888d7a58
Vamsi <yechoorv@gmail.com>
2017-03-31 01:03:17 -0400

    cleaned code

--- secure/home.php
+++ secure/home.php
@@ -22,7 +22,7 @@ $affiliation = $headers['affiliation'];
   <script type="text/javascript" src="/js/dashboard.controller.js"></script>
 </head>
 <body>
-  <div ng-app="Dashboard" ng-controller="DashboardController" ng-init="onyen='<?php echo $onyen; ?>'; pid='<?php echo $pid; ?>'; firstName = '<?php echo $firstName; ?>'; lastName = '<?php echo $lastName; ?>'; email = '<?php echo $email; ?>'; affiliation = '<?php echo $affiliation; ?>'; setAccess(); getAttendance();">
+  <div ng-app="Dashboard" ng-controller="DashboardController" ng-init="onyen='<?php echo $onyen; ?>'; pid='<?php echo $pid; ?>'; firstName = '<?php echo $firstName; ?>'; lastName = '<?php echo $lastName; ?>'; email = '<?php echo $email; ?>'; affiliation = '<?php echo $affiliation; ?>'; setAccess();">
     <nav class="navbar navbar-default">
       <div class="container-fluid">
         <div class="navbar-header">
@@ -39,27 +39,34 @@ $affiliation = $headers['affiliation'];
         </ul>
       </div>
     </nav>
-    <ul id="courseTabs" class="nav nav-tabs">
-      <li class="active"><a href="#home" data-toggle="tab">Home</a></li>
-      <li ng-repeat="(key, value) in tabs"><a ng-href="#{{key}}" data-toggle="tab">{{key}}</a></li>
-    </ul>
-    <div class="tab-content">
-      <div class="tab-pane active" id="home" name="home">
-        <h2>Info:</h2>
-        <ul>
-          <li>{{onyen}}</li>
-          <li>{{pid}}</li>
-          <li>{{firstName}} {{lastName}}</li>
-          <li>{{email}}</li>
-          <li>{{affiliation}}</li>
+    <section>
+      <div ng-init="getAttendance()">
+        <ul id="courseTabs" class="nav nav-tabs">
+          <li class="active"><a href="#home" data-toggle="tab">Home</a></li>
+          <li ng-repeat="(key, value) in tabs"><a ng-href="#{{key}}" data-toggle="tab">{{key}}</a></li>
         </ul>
+        <div class="tab-content">
+          <div class="tab-pane active" id="home" name="home">
+            <h2>Info:</h2>
+            <ul>
+              <li>{{onyen}}</li>
+              <li>{{pid}}</li>
+              <li>{{firstName}} {{lastName}}</li>
+              <li>{{email}}</li>
+              <li>{{affiliation}}</li>
+            </ul>
+          </div>
+          <div ng-repeat="(key, value) in tabs" id="{{key}}" name="{{key}}" class="tab-pane">
+            <h2>{{key}} Attendance:</h2>
+            <h5>{{value.attendance}}</h5>
+            <h5>{{value.records}}</h5>
+          </div>
+        </div>
       </div>
-      <div ng-repeat="(key, value) in tabs" id="{{key}}" name="{{key}}" class="tab-pane">
-        <h2>{{key}} Attendance:</h2>
-        <h5>{{value.attendance}}</h5>
-        <h5>{{value.records}}</h5>
-      </div>
-    </div>
+    </section>
+    <section>
+      
+    </section>
   </div>
   <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
