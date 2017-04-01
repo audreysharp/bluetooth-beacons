a20f3c3d2219d3ae51584fa63043769c385fba6e
Vamsi <yechoorv@gmail.com>
2017-03-31 23:58:05 -0400

    fixed bug in DBFunctions

--- backend/include/DBFunctions.php
+++ backend/include/DBFunctions.php
@@ -21,8 +21,9 @@ class DBFunctions {
 	public function addCheckIn($onyen, $role, $course_dept, $course_num, $course_sec) {
 		$db = $this->__construct();
 
-		$course_id = $db->query("SELECT sno FROM courses WHERE department = '$course_dept' AND number = '$course_num' AND section = '$course_sec'") or die(mysqli_error());
-		if($course_id){
+		$query_id = $db->query("SELECT sno FROM courses WHERE department = '$course_dept' AND number = '$course_num' AND section = '$course_sec'") or die(mysqli_error());
+		if($query_id && $query_id->num_rows > 0){
+			$course_id = $course_id->fetch_array();
 			$query = $db->query("INSERT INTO attendance(onyen, course_id), timestamp) VALUES('$onyen', '$course_id', CURRENT_TIMESTAMP())") or die(mysqli_error());
 			if($query) {
 				// Successful insert
